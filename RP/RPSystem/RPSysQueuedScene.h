9485867d24bbe76d79a239dc3a28b4a0274ab449
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-18 11:42:25 -0500

    Document RPSysQueuedScene

--- RP/RPSystem/RPSysQueuedScene.h
+++ RP/RPSystem/RPSysQueuedScene.h
@@ -5,31 +5,40 @@
 #include <types_egg.h>
 
 /**
- * @brief Represents the next scene that will be loaded.
- * 
- * Used commonly by the menu scene to determine whether the player select scene
+ * @brief Tracks the next scene that will be loaded
+ * @note Used commonly by the menu scene to determine whether the player select scene
  * should be in its regular/training/fitness state.
- * 
- * (Custom name)
+ * @customname
  */
 class RPSysQueuedScene
 {
 public:    
-    static RPSysQueuedScene * CreateInstance(EGG::Heap *heap); // 80187ba0
+    //! @address 80187ba0
+    static RPSysQueuedScene * CreateInstance(EGG::Heap *heap);
     static RPSysQueuedScene * getInstance() { return sInstance; }
 
-    void Reset(); // 80187b94
+    /**
+     * Reset the queued scene to -1
+     * @address 80187b94
+     */
+    void Reset();
 
 private:
     RPSysQueuedScene(EGG::Heap *heap) : mParentHeap(heap), mSceneID(-1) {}
-    virtual ~RPSysQueuedScene(); // 80187b54
+    //! @address 80187b54
+    virtual ~RPSysQueuedScene();
 
 private:
+    //! Heap in which this object was allocated
     EGG::Heap *mParentHeap; // at 0x4
-    //! @brief ID of queued scene
+    //! ID of queued scene (-1 = none)
     s32 mSceneID; // at 0x8
 
-    static RPSysQueuedScene *sInstance; // 804bf4f8
+    /**
+     * Static instance
+     * @address 804bf4f8
+     */
+    static RPSysQueuedScene *sInstance;
 };
 
 #endif
