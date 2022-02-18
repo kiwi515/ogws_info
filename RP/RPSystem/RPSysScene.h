d930b11c93a423a55e16e0f3989fadbb2d2b8163
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-02-18 16:18:21 -0500

    Document RPSysScene

--- RP/RPSystem/RPSysScene.h
+++ RP/RPSystem/RPSysScene.h
@@ -9,110 +9,208 @@
 
 /**
  * @brief Base class for all RP engine scenes.
- * 
- * Inherits from EGG::Scene for compatability,
+ * @note Inherits from EGG::Scene for compatability,
  * and inherits from a renderable interface to simplify the renderer's job.
- * 
- * (Name from Wii Fit U)
+ * @wfuname
  */
 class RPSysScene : EGG::Scene, IRPGrpDrawObject
 {
 public:
+    //! @address 80185404
     RPSysScene();
-    virtual ~RPSysScene() {} // 80183fa8
-    virtual void calc(); // 80185094
-    virtual void draw(); // 80185048
-    virtual void enter(); // 80185304
-    virtual void exit(); // 80184f90
-    virtual void reinit(); // 8018522c
-    virtual void incoming_childDestroy(); // 80185168
-    virtual void outgoing_childCreate(); // 80185008
-    //! Returns address of some unused global object
-    virtual UNKTYPE * VF38(UNKTYPE); // 8018558c
-    //! Callback for pause/unpause (bool true if pausing)
-    virtual void pauseCallBack(bool pauseOn); // 80185594
-    //! Initialize/setup the scene
-    virtual void Configure(); // 80185400
-    //! Load any needed resources
-    virtual void LoadResource(); // 80185598
-    //! Re-initialize the scene
-    virtual void Reset(); // 80185228
-    //! Run scene logic
-    virtual void Calculate(); // 80185164
-    //! Exit the scene
-    virtual void Exit(); // 80185004
-    //! @brief Async task
-    //! Called through taskAsyncFunc
-    virtual void taskAsync(); // 8018559c
+    //! @address 80183fa8
+    virtual ~RPSysScene() {}
+    //! @address 80185094
+    virtual void calc();
+    //! @address 80185048
+    virtual void draw();
+    //! @address 80185304
+    virtual void enter();
+    //! @address 80184f90
+    virtual void exit();
+    //! @address 8018522c
+    virtual void reinit();
+    //! @address 80185168
+    virtual void incoming_childDestroy();
+    //! @address 80185008
+    virtual void outgoing_childCreate();
+    /**
+     * Returns address of DAT_804bf4d0
+     * @address 8018558c
+     */
+    virtual UNKTYPE * VF38(UNKTYPE);
+    /**
+     * Callback for pause/unpause
+     * @address 80185594
+     * @param pauseOn True = enter, false = exit
+     */
+    virtual void pauseCallBack(bool pauseOn);
+    /**
+     * Setup scene members
+     * @address 80185400
+     */
+    virtual void Configure();
+    /**
+     * Load required scene resources
+     * @address 80185598
+     */
+    virtual void LoadResource();
+    //! @address 80185228
+    virtual void Reset();
+    //! @address 80185164
+    virtual void Calculate();
+    //! @address 80185004
+    virtual void Exit();
+    /**
+     * @brief Async task
+     * @note Called by taskAsyncFunc
+     * @address 8018559c
+     */
+    virtual void taskAsync();
 
-    //! Disable fade-in when entering/re-initializing the scene
-    void disableFadeIn(); // 80184e80
-    //! Creator scene ID
-    int getCreatorSceneID() const; // 80184e90
-    void setCreatorSceneID(int scene); // 80184e98
-    //! Initialize scene members
-    void init(); // 80184ea0
-    //! Set VI dimming
-    void setDimming(bool dim); // 8018546c
-    //! @brief Control scene fader
-    //! Bool is used in WFU, but these have the scene mgr do it,
-    //! rather than accessing the fader directly
-    void startFadeOut(bool); // 80185474
-    void startFadeIn(bool); // 8018547c
-    //! Set manager's scene fade color
-    void setSceneFadeColor(EGG::Color color); // 80185484
-    //! Fade color of both of manager's faders
-    void setFadeColor(EGG::Color color); // 80185484
-    EGG::Color getFadeColor() const; // 80185514
-    //! Set fade frame of both of manager's faders
-    void setFadeFrame(u16 frame); // 8018551c
-    //! @brief If it is safe for the console NAND to be accessed
-    //! The scene manager must not be fading in or be busy
-    bool isNandAccessEnable() const; // 80185524
-    //! @brief If it is okay for the scene to be changed
-    //! The manager must be ready to fade out and not be busy
-    bool isSceneChangeEnable() const; // 80185574
-    //! @brief If the scene is currently blank (not displayed at all)
-    //! This happens when both faders are preparing to fade in
-    bool isSceneBlank() const; // 8018557c
-    //! @brief If the scene is currently fully displayed
-    //! This happens when both faders are done and ready to fade back out
-    bool isSceneDisplay() const; // 80185584
+    /**
+     * Disable scene fade-in on enter/reset
+     * @address 80184e80
+     */
+    void disableFadeIn();
 
-    /* Functions below defined in RPSysLoadScene.cpp */
+    //! @address 80184e90
+    int getCreatorSceneID() const;
+    //! @address 80184e98
+    void setCreatorSceneID(int scene);
 
-    //! @brief Calls virtual task function
-    //! Set up on thread by initTaskAsync
-    static void taskAsyncFunc(void *scene); // 80186878
-    //! Calls LoadResource when done
-    static void loadResourceFunc(void *scene); // 80186888
+    /**
+     * Initialize base RP scene members
+     * @address 80184ea0
+     */
+    void init();
+
+    /**
+     * Toggle VI dimming
+     * @address 8018546c
+     */
+    void setDimming(bool dim);
+
+    /**
+     * @note Bool goes used in WFU, but OGWS just tells the manager
+     * rather than accessing the fader directly
+     */
+    /**@{*/ 
+    //! @address 80185474
+    void startFadeOut(bool);
+    //! @address 8018547c
+    void startFadeIn(bool);
+    /**@}*/
+
+    /**
+     * @brief Set manager's scene fade color
+     * @address 80185484
+     */
+    void setSceneFadeColor(EGG::Color color);
+    /**
+     * @brief Fade color of both of manager's faders
+     * @note Manager has a scene fader and a manager fader
+     */
+    /**@{*/ 
+    //! @address 801854cc
+    void setFadeColor(EGG::Color color);
+    //! @address 80185514
+    EGG::Color getFadeColor() const;
+    /**@}*/
+
+    /**
+     * @brief Set fade frame of both of manager's faders
+     * @address 8018551c
+     */
+    void setFadeFrame(u16 frame);
+
+    /**
+     * @brief Check if the scene manager is allowing NAND access
+     * @note The scene manager must not be fading in or be busy
+     * @address 80185524
+     */
+    bool isNandAccessEnable() const;
+    /**
+     * @brief Check if the scene manager is allowing the scene to change
+     * @note The scene manager must be ready to fade out and not be busy
+     * @address 80185574
+     */
+    bool isSceneChangeEnable() const;
+
+    /**
+     * @brief Check if the scene is currently blank (not visible)
+     * @note This happens when both faders are preparing to fade in
+     * @address 8018557c
+     */
+    bool isSceneBlank() const;
+    /**
+     * @brief Check if the scene is currently fully visible
+     * @note This happens when both faders are done and ready to fade back out
+     * @address 80185584
+     */
+    bool isSceneDisplay() const;
+
+    /**
+     * @name Defined in RPSysLoadScene.cpp
+     */
+    /**@{*/ 
+    /**
+     * @brief Calls virtual task function asynchronously
+     * @note Set up on thread by initTaskAsync
+     * @address 80186878
+     * @param scene Scene required to call taskAsync
+     */
+    static void taskAsyncFunc(void *scene);
+    /**
+     * @brief Loads scene resources asynchronously
+     * @note calls LoadResource when done
+     * @address 80186888
+     * @param scene Scene required to call taskAsync
+     */
+    static void loadResourceFunc(void *scene);
 
     UNKTYPE FUN_80186960(UNKTYPE);
     UNKTYPE FUN_80186a1c(UNKTYPE);
 
-    //! Request DVD thread to run taskAsyncFunc
-    void initTaskAsync(); // 80186aa8
+    /**
+     * @brief Request DVD thread to run taskAsyncFunc
+     * @address 80186aa8
+     */
+    void initTaskAsync();
 
-    UNKTYPE FUN_80186b20(UNKTYPE); // 80186b20
-    UNKTYPE FUN_80186c34(UNKTYPE); // 80186c34
+    UNKTYPE FUN_80186b20(UNKTYPE);
+    UNKTYPE FUN_80186c34(UNKTYPE);
 
-    //! @brief Enable the "Now Loading" scene text
-    //! Set automatically if the scene attributes allow it
-    void setShowLoadingText(); // 80186cec
-    //! If the scene is waiting on an async task still
-    bool isTaskAsyncFinish() const; // 80186cfc
-    //! Set waiting on async flag
-    void setTaskAsync(); // 80186d1c
+    /**
+     * @brief Enable the "Now Loading" scene text
+     * @note Set automatically if the scene attributes allow it
+     * @address 80186cec
+     */
+    void setShowLoadingText();
+    /**
+     * @brief Check if the scene is waiting on an async task
+     * @address 80186cfc
+     */
+    bool isTaskAsyncFinish() const;
+    /**
+     * @brief Set waiting on async flag
+     * @address 80186d1c
+     */
+    void setTaskAsync();
 
     UNKTYPE FUN_80186d2c(UNKTYPE);
 
-    //! Print time stamp string in specified color
-    void printTimeStamp(EGG::Color color); // 80186d30
+    /**
+     * @brief Print time stamp string in specified color
+     * @address 80186d30
+     */
+    void printTimeStamp(EGG::Color color);
+    /**@}*/
 
 private:
-    //! Becomes the global renderer when scene is entered
+    //! Scene renderer (becomes global on scene enter)
     RPGrpRenderer *mRenderer; // at 0x2C
-    //! Not sure what purpose these serve yet
+    //! Common draw functionality
     RPSysCommonObject *mCmnObject; // at 0x30
     //! @brief TODO, constructor at 801988d8
     //! Some particle related structure
@@ -133,7 +231,7 @@ private:
     //! VF38 will return its address
     static UNKWORD DAT_804bf4d0; // 804bf4d0
 
-    //! @brief Defined in RPSysLoadScene.cpp
+    //! Defined in RPSysLoadScene.cpp
     static EGG::Vector3f DAT_804a3db0[3]; // 804a3db0
 };
 
