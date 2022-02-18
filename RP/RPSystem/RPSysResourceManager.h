164b3056e9a7e54176133d12bd9da06cb617cd14
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-02-18 14:40:05 -0500

    Document RPSysResourceManager

--- RP/RPSystem/RPSysResourceManager.h
+++ RP/RPSystem/RPSysResourceManager.h
@@ -9,54 +9,116 @@
 
 /**
  * @brief Engine resource manager
- * 
- * Responsible for pretty much all file loads from the disc,
+ * @note Responsible for pretty much all file loads from the disc,
  * and also serves as a utility for building scene/pack relative paths to files.
- * 
- * (Name from Wii Fit U)
+ * @wfuname
  */
 class RPSysResourceManager
 {
 public:
-    //! Singleton methods
-    static RPSysResourceManager * CreateInstance(); // 80187ae8
+    //! @address 80187ae8
+    static RPSysResourceManager * CreateInstance();
     static RPSysResourceManager * GetInstance() { return sInstance; }
 
-    //! Open binary message (BMG) file
-    static void * GetMessageResource(const char *path); // 8018783c
-    //! Get file from EGG archive (optionally get file size)
-    static void * GetFileFromArchive(EGG::Archive *arc, const char *path, u32 *sizeOut); // 80187878
-    //! Load archive from current scene's Stage directory
-    static EGG::Archive * LoadGameStageArchive(const char *path); // 801878c4
-    //! Load local archive (local.carc) for the specified scene
-    static EGG::Archive * LoadGameLocalArchive(int sceneID, EGG::Heap *heap); // 80187964
-    //! Load common archive (common.carc) for the specified scene
-    static EGG::Archive * LoadGameCommonArchive(int sceneID, EGG::Heap *heap); // 80187a0c
+    /**
+     * Open binary message (BMG) file 
+     * @address 8018783c
+     */
+    static void * GetMessageResource(const char *path);
+    /**
+     * Get file from EGG archive
+     * @address 80187878
+     * @param sizeOut Optional output pointer for file size
+     */
+    static void * GetFileFromArchive(EGG::Archive *arc, const char *path, u32 *sizeOut);
+    /**
+     * Load archive from current scene's Stage directory
+     * @address 801878c4
+     */
+    static EGG::Archive * LoadGameStageArchive(const char *path);
+    /**
+     * Load local archive (`local.carc`) for the specified scene
+     * @address 80187964
+     */
+    static EGG::Archive * LoadGameLocalArchive(int sceneID, EGG::Heap *heap);
+    /**
+     * Load common archive (`common.carc`) for the specified scene
+     * @address 80187a0c
+     */
+    static EGG::Archive * LoadGameCommonArchive(int sceneID, EGG::Heap *heap);
 
-    //! Load file from DVD (optionally get file size)
-    void * LoadFromDVD(const char *path, EGG::Heap *heap, int allocDir, int *sizeOut); // 80186eb4
-    //! Load and decompress file
-    void * LoadCompressed(const char *path, EGG::Heap *heap); // 80186fa0
-    //! Load local archive in specified directory
-    EGG::Archive * LoadLocalArchive(const char *dir); // 801871f4
-    //! Get local sound path of specified scene
-    void GetGameSoundLocalPath(char *bufOut, u32 bufSize, int sceneID); // 80187290
-    //! Get common sound path of specified scene
-    void GetGameSoundCommonPath(char *bufOut, u32 bufSize, int sceneID); // 80187300
-    //! Get local sound path of current pack's static directory
-    void GetStaticSoundLocalPath(char *bufOut, u32 bufSize); // 8018736c
-    //! Get common sound path of current pack's static directory
-    void GetStaticSoundCommonPath(char *bufOut, u32 bufSize); // 801873e8
-    //! Load Mii resources (kokeshi.carc)
-    void LoadKokeshiArchive(); // 8018744c
-    //! Load static common archive
-    void LoadSharedCommonArchive(); // 801874b4
-    //! Load cached archives (font, BMG, static common)
-    void LoadCacheArchives(); // 80187604
-    //! Remove file from file list
-    void RemoveFromFileList(RPSysFile *file); // 801877d4
-    //! Load sound archive
-    bool LoadSoundArchive(const char *path); // 801877dc
+    /**
+     * Load file from DVD
+     * @address 80186eb4
+     * @param sizeOut Optional output pointer for file size
+     */
+    void * LoadFromDVD(const char *path, EGG::Heap *heap, int allocDir, int *sizeOut);
+    /**
+     * Load and decompress file
+     * @address 80186fa0
+     */
+    void * LoadCompressed(const char *path, EGG::Heap *heap);
+    /**
+     * Load local archive in specified directory
+     * @address 801871f4
+     */
+    EGG::Archive * LoadLocalArchive(const char *dir);
+
+    /**
+     * Get local sound path of specified scene
+     * @address 80187290
+     * @param bufOut Output buffer for path
+     * @param bufSize Size of output buffer (unused)
+     */
+    void GetGameSoundLocalPath(char *bufOut, u32 bufSize, int sceneID);
+    /**
+     * Get common sound path of specified scene
+     * @address 80187300
+     * @param bufOut Output buffer for path
+     * @param bufSize Size of output buffer (unused)
+     */
+    void GetGameSoundCommonPath(char *bufOut, u32 bufSize, int sceneID);
+    /**
+     * Get local sound path of current pack's static directory
+     * @address 8018736c
+     * @param bufOut Output buffer for path
+     * @param bufSize Size of output buffer (unused)
+     */
+    void GetStaticSoundLocalPath(char *bufOut, u32 bufSize);
+    /**
+     * Get common sound path of current pack's static directory
+     * @address 801873e8
+     * @param bufOut Output buffer for path
+     * @param bufSize Size of output buffer (unused)
+     */
+    void GetStaticSoundCommonPath(char *bufOut, u32 bufSize);
+
+    /**
+     * Load Mii resources (`kokeshi.carc`)
+     * @address 8018744c
+     */
+    void LoadKokeshiArchive();
+    /**
+     * Load static common archive
+     * @address 801874b4
+     */
+    void LoadSharedCommonArchive();
+    /**
+     * Load cached archives (font, BMG, static common)
+     * @address 80187604
+     */
+    void LoadCacheArchives();
+    /**
+     * Load sound archive
+     * @address 801877dc
+     */
+    bool LoadSoundArchive(const char *path);
+
+    /**
+     * Remove file from file list
+     * @address 801877d4
+     */
+    void RemoveFromFileList(RPSysFile *file);
 
 private:
     RPSysResourceManager()
@@ -65,24 +127,38 @@ private:
         nw4r::ut::List_Init(&mFileList, offsetof(RPSysFile, mNode));
         mPath = new char[128];
     }
-    virtual ~RPSysResourceManager(); // 80187aa8
+    //! @address 80187aa8
+    virtual ~RPSysResourceManager();
 
     nw4r::ut::List LIST_0x4;
     nw4r::ut::List mFileList; // at 0x10
+    //! Buffer for constucting path
     char *mPath; // at 0x1C
+    //! Kokeshi (Mii) archive
     EGG::Archive *mKokeshiArc; // at 0x20
+    //! Message (BMG) archive
     EGG::Archive *mMessageArc; // at 0x24
+    //! Font archive
     EGG::Archive *mFontArc; // at 0x28
-    EGG::Archive *mGameCommonArc; // at 0x2C
-    EGG::Archive *mGameLocalArc; // at 0x30
+    //! Pack static common archive
+    EGG::Archive *mStaticCmnArc; // at 0x2C
+    //! Pack static local archive
+    EGG::Archive *mStaticLocalArc; // at 0x30
     UNKWORD WORD_0x34;
     UNKTYPE *PTR_0x38;
     UNKWORD WORD_0x3C;
 
-    //! Static instance
-    static RPSysResourceManager *sInstance; // 804bf4f0
-    //! Pack Project static paths
-    static const char *sPackStaticPaths[4]; // 803b9850
+    /**
+     * Static instance
+     * @address 804bf4f0
+     */
+    static RPSysResourceManager *sInstance;
+
+    /**
+     * Pack Project static paths
+     * @address 803b9850
+     */
+    static const char *sPackStaticPaths[4];
 };
 
 const char *RPSysResourceManager::sPackStaticPaths[4] = 
