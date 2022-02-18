ede2d8e5885a59af18c637c51bab29a44a65167b
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-02-18 14:53:18 -0500

    Document RPSysSaveData

--- RP/RPSystem/RPSysSaveData.h
+++ RP/RPSystem/RPSysSaveData.h
@@ -5,55 +5,71 @@
 #include <types_egg.h>
 
 /**
- * @brief Structure to hold RP engine save data
- * 
- * While this structure is generic and can support multiple Pack Project games,
- * it seems that its behavior is set at compile time (along with getFileSize),
- * rather than using RPSysProjectLocal.
- * 
- * (Custom name, derived from some Wii Fit U symbols)
+ * @brief Generic structure to hold RP engine save data
+ * @note While this structure is generic and can support multiple Pack Project games,
+ * it seems that some of its behavior is set at compile time rather than using RPSysProjectLocal.
+ * @wfuname
  */
 class RPSysSaveData
 {
 public:
-    /**
-     * @brief Potential errors in the save data
-     */
+    //! @brief Potential errors in the save data
     enum ESaveError
     {
-        SAVE_NO_HI_MAGIC = (1 << 0),
-        SAVE_NO_LO_MAGIC = (1 << 1),
+        //! Missing 'RPSP'
+        SAVE_BAD_HI_MAGIC = (1 << 0),
+        //! Missing '0000'
+        SAVE_BAD_LO_MAGIC = (1 << 1),
         SAVE_BAD_CHECKSUM = (1 << 2)
     };
 
 public:
-    //! Total size of raw save file
-    static u32 getFileSize(); // 8018bf54
+    /**
+     * Size of save file
+     * @address 8018bf54
+     */
+    static u32 getFileSize();
 
-    RPSysSaveData(); // 8018ad34
+    //! @address 8018ad34
+    RPSysSaveData();
 
-    //! @brief For serialization (From WFU)
-    //! Raw save is needed to calculate checksum
-    void write(EGG::RamStream *stream, const void *rawSave) const; // 8018ad70
-    void read(EGG::RamStream *stream, const void *rawSave); // 8018b0e4
+    /**
+     * Write save to stream
+     * @address 8018ad70
+     * @param rawSave Binary save, needed to calculate checksum
+     */
+    void write(EGG::RamStream *stream, const void *rawSave) const;
+    /**
+     * Read save from stream
+     * @address 8018b0e4
+     * @param rawSave Binary save, needed to calculate checksum
+     */
+    void read(EGG::RamStream *stream, const void *rawSave);
 
-    //! Wii Play common save data
-    RPPartyCommonData *getPartyCommonData() const; // 8018c32c
+    //! @address 8018c32c
+    RPPartyCommonData *getPartyCommonData() const;
 
-    //! Wii Play player list data
-    void setPartyPlayerData(const RPPartyPlayerData *playerData, u32 id); // 8018c334
-    RPPartyPlayerData *getPartyPlayerData(u32 id) const; // 8018c40c
+    //! @address 8018c334
+    void setPartyPlayerData(const RPPartyPlayerData *playerData, u32 id);
+    //! @address 8018c40c
+    RPPartyPlayerData *getPartyPlayerData(u32 id) const;
 
-    //! Wii Sports common data
-    void setSportsCommonData(const RPSportsCommonData *cmnData); // 8018c41c
-    RPSportsCommonData *getSportsCommonData() const; // 8018c684
+    //! @address 8018c41c
+    void setSportsCommonData(const RPSportsCommonData *cmnData);
+    //! @address 8018c684
+    RPSportsCommonData *getSportsCommonData() const;
 
-    //! Wii Sports player list data
-    void setSportsPlayerData(const RPSportsPlayerData *playerData, u32 id); // 8018c68c
-    RPSportsPlayerData *getSportsPlayerData(u32 id) const; // 8018cb6c
+    //! @address 8018c68c
+    void setSportsPlayerData(const RPSportsPlayerData *playerData, u32 id);
+    //! @address 8018cb6c
+    RPSportsPlayerData *getSportsPlayerData(u32 id) const;
 
-    //! Save data validity (typo)
-    bool isErrorOccured() const; // 8018cb7c
+    /**
+     * Check save data validity
+     * @typo
+     * @address 8018cb7c
+     */
+    bool isErrorOccured() const;
 
 private:
     //! Flags regarding the status of the data
@@ -67,7 +83,6 @@ private:
     //! Wii Play player list (100 entries)
     RPPartyPlayerData *mPartyPlayerList; // at 0x10
 
-    // TO-DO: Other Pack Project game saves?
     UNKWORD WORD_0x14;
     UNKWORD WORD_0x18;
     UNKWORD WORD_0x1C;
