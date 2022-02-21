af0527b6b4419e9f63eda124190c2cf1a53eb7cf
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-21 10:49:32 -0500

    Document IRPSysHostIOSocket

--- RP/RPKernel/IRPSysHostIOSocket.h
+++ RP/RPKernel/IRPSysHostIOSocket.h
@@ -3,18 +3,26 @@
 #include "types_RP.h"
 
 /**
- * @brief Seems to be functionality for IO while debugging?
- * All of its functions have been stubbed out.
+ * @brief Host I/O for debugging
+ * @details All functions have been stubbed out
  */
 class IRPSysHostIOSocket
 {
 public:
-    virtual ~IRPSysHostIOSocket(); // 80190794
-    virtual UNKTYPE FUN_8018a904(UNKTYPE) {} // 8018a904
-    virtual UNKTYPE FUN_8018a908(UNKTYPE) {} // 8018a908
+    //! @address 80190794
+    virtual ~IRPSysHostIOSocket();
+    //! @address 8018a904
+    virtual UNKTYPE FUN_8018a904(UNKTYPE) {}
+    //! @address 8018a908
+    virtual UNKTYPE FUN_8018a908(UNKTYPE) {}
     
-    IRPSysHostIOSocket(const char *, ...); // 801907e4
-    IRPSysHostIOSocket(); // 801907d4
+    /**
+     * Construct socket with formattable name
+     * @address 801907e4
+     */
+    IRPSysHostIOSocket(const char *name, ...);
+    //! @address 801907d4
+    IRPSysHostIOSocket();
 };
 
 #endif
