0671b7dfa6fd163d83d11dac9bb807384c43a300
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-21 11:10:49 -0500

    Document RPSysFile

--- RP/RPKernel/RPSysFile.h
+++ RP/RPKernel/RPSysFile.h
@@ -6,10 +6,11 @@
 #include <egg/core/eggDisposer.h>
 
 /**
- * @brief Similar to EGG's DvdFile structure,
+ * @brief Disc file loaded into RAM
+ * @details Similar to EGG's DvdFile structure,
  * except this class expects the file's data to already have been loaded into RAM.
  * 
- * Paths are relative to the project root, i.e. "Common/Kokeshi/common.carc"
+ * Paths are relative to the project root (i.e. `"Common/Kokeshi/common.carc"`)
  * 
  * In theory this class could be used for all open files, but the resource manager
  * seems to only use it for archives post-decompression.
@@ -18,19 +19,30 @@
  * This class has an additional ut::Node so that the resource manager can keep track of all open files.
  * It removes itself from the resource manager's file list (at 0x10) on destruction.
  * 
- * (Name from Wii Fit U)
+ * @wfuname
  */
 class RPSysFile : EGG::Disposer
 {
 public:
-    RPSysFile(const char *path, int size, const void *data); // 80199678
-    virtual ~RPSysFile(); // 80199604
+    /**
+     * @address 80199678
+     * @param path File path (relative to project root)
+     * @param size File size
+     * @param data Pointer to file data
+     */
+    RPSysFile(const char *path, int size, const void *data);
+    //! @address 80199604
+    virtual ~RPSysFile();
 
 public:
+    //! Link node for resource manager
     nw4r::ut::Node mNode; // at 0x10
 private:
+    //! File path
     char mPath[128]; // at 0x18
+    //! File size
     int mSize; // at 0x98
+    //! File data
     void *mData; // at 0x9C
 };
 
