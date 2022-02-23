fcc186a148899f963debbc2fbd9048e98ca4e4e1
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-23 11:27:20 -0500

    Document RPSysTextWriter

--- RP/RPKernel/RPSysTextWriter.h
+++ RP/RPKernel/RPSysTextWriter.h
@@ -8,29 +8,46 @@
 
 /**
  * @brief RP engine screen text writer
- * 
- * Provides base functionality from NW4R, but with some extra features to make printing easier,
+ * @details Provides base functionality from NW4R, but with some extra features to make printing easier,
  * such as the ability to print to an arbitrary screenspace location, or begin/end rendering.
- * 
- * (Name from Wii Fit U)
+ * @wfuname
  */
 class RPSysTextWriter : nw4r::ut::TextWriterBase<char>
 {
 public:
-    //! Singleton methods
-    static RPSysTextWriter * CreateInstance(); // 80190cc4
+    //! @address 80190cc4
+    static RPSysTextWriter * CreateInstance();
     static RPSysTextWriter * GetInstance() { return sInstance; }
 
-    //! Reset all settings
-    void Reset(); // 80190b9c
-    //! Format text and print to screen coordinates
-    void Printf(f32 x, f32 y, const char *msg, ...); // 80190af8
-    //! Format text and print to screen coordinates, with the origin at the screen center
-    void PrintfZeroCenter(f32 x, f32 y, const char *msg, ...); // 80190960
-    //! Setup and begin rendering
-    void Begin(); // 801908d8
-    //! End rendering
-    void End(); // 801908cc
+    /**
+     * Reset all settings
+     * @address 80190b9c
+     */
+    void Reset();
+
+    /**
+     * Print formatted text to screen coordinates (relative to the top-left)
+     * @address 80190af8
+     */
+    void Printf(f32 x, f32 y, const char *msg, ...);
+
+    /**
+     * Print formatted text to screen coordinates (relative to the center)
+     * @address 80190960
+     */
+    void PrintfZeroCenter(f32 x, f32 y, const char *msg, ...);
+
+    /**
+     * Setup GX for rendering
+     * @address 801908d8
+     */
+    void Begin();
+    
+    /**
+     * End rendering
+     * @address 801908cc
+     */
+    void End();
 
 private:
     RPSysTextWriter()
@@ -38,14 +55,18 @@ private:
         SetFont(RPSysFontManager::GetInstance()->GetRomFont());
         mIsRendering = FALSE;
     }
-    virtual ~RPSysTextWriter(); // 80190c6c
+    //! @address 80190c6c
+    virtual ~RPSysTextWriter();
 
 private:
     //! Render status (Begin/End)
     BOOL mIsRendering; // at 0x68
 
-    //! Static instance
-    static RPSysTextWriter *sInstance; // 804bf538
+    /**
+     * Static instance
+     * @address 804bf538
+     */
+    static RPSysTextWriter *sInstance;
 };
 
 #endif
