357bb7fb232ed55766dcb9b64f037a341e873e81
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-23 15:50:56 -0500

    Document RPSportsCommonData

--- RP/RPSports/RPSportsCommonData.h
+++ RP/RPSports/RPSportsCommonData.h
@@ -6,117 +6,141 @@
 
 /**
  * @brief Wii Sports save file common data
- * This includes everything that isn't tied to a specific player
- * 
- * NOTE: Not a packed structure, this is an abstraction of the binary format
- * 
- * (Custom name, derived from some Wii Fit U symbols)
+ * @details Includes everything that isn't tied to a specific player
+ * @note Not a packed structure, this is an abstraction of the binary format
+ * @customname
  */
 class RPSportsCommonData
 {
 public:
     /**
      * @brief Structure used to allow players' Miis to be automatically selected.
-     * 
-     * The Mii's player list index is kept alongside information identifying the remote used,
+     * @details The Mii's player list index is kept alongside information identifying the remote used,
      * so that Mii can be chosen automatically when applicable.
      */
     struct MiiHistory
     {
         //! @brief Previously selected Mii (Index into player list)
-        //! -1 if unused
+        //! @note -1 if unused
         s8 mPrevMiiIdx; // at 0x0
 
         //! @brief Set by WPADGetRadioSensitivity
-        //! Used to identify the remote previously used
+        //! @details Used to identify the remote previously used
         u8 mRadioSensitivity[6]; // at 0x1
     };
 
     /**
      * @brief Indices for the common save data bitfield
-     * 
-     * Must be used with getTrainingUnlock, or getBitFlag(15 + bit)
+     * @note Must be used with getTrainingUnlock, or getBitFlag(15 + bit)
      */
-    enum ETrainingBits
+    enum ETrainingGames
     {
-        // Throwing Punches
-        BIT_BOX_MITUCHI,
-        // Dodging
-        BIT_BOX_TAMAYOKE,
-        // Working the Bag
-        BIT_BOX_SANDBAG,
+        BIT_BOX_MITUCHI,    //!< Throwing Punches
+        BIT_BOX_TAMAYOKE,   //!< Dodging
+        BIT_BOX_SANDBAG,    //!< Working the Bag
 
-        // Target Practice
-        BIT_GOL_TARGET,
-        // Hitting the Green
-        BIT_GOL_NEARPIN,
-        // Putting
-        BIT_GOL_ONEPAT,
+        BIT_GOL_TARGET,     //!< Target Practice
+        BIT_GOL_NEARPIN,    //!< Hitting the Green
+        BIT_GOL_ONEPAT,     //!< Putting
 
-        // Spin Control
-        BIT_BOW_GATETOSHI,
-        // Power Throws
-        BIT_BOW_NAGITAOSHI,
-        // Picking Up Spares
-        BIT_BOW_SPAREGET,
+        BIT_BOW_GATETOSHI,  //!< Spin Control
+        BIT_BOW_NAGITAOSHI, //!< Power Throws
+        BIT_BOW_SPAREGET,   //!< Picking Up Spares
 
-        // Batting Practice
-        BIT_BSB_RENZOKU,
-        // Swing Control
-        BIT_BSB_UCHIWAKE,
-        // Hitting Home Runs
-        BIT_BSB_HOMERUN,
+        BIT_BSB_RENZOKU,    //!< Batting Practice
+        BIT_BSB_UCHIWAKE,   //!< Swing Control
+        BIT_BSB_HOMERUN,    //!< Hitting Home Runs
 
-        // Target Practice
-        BIT_TNS_KABEUCHI,
-        // Timing Your Swing
-        BIT_TNS_NERAUCHII,
-        // Returning Balls
-        BIT_TNS_RENZOKU,
+        BIT_TNS_KABEUCHI,   //!< Target Practice
+        BIT_TNS_NERAUCHII,  //!< Timing Your Swing
+        BIT_TNS_RENZOKU,    //!< Returning Balls
     };
 
 public:
     RPSportsCommonData(); // 8018ad34
 
-    //! For serialization (From WFU)
-    void write(EGG::RamStream *stream) const; // 8018a90c
-    void read(EGG::RamStream *stream); // 8018a9d0
+    /**
+     * @brief Write data
+     * @address 8018a90c
+     */
+    void write(EGG::RamStream *stream) const;
+    /**
+     * @brief Read data
+     * @address 8018a9d0
+     */
+    void read(EGG::RamStream *stream);
 
-    //! Access Mii history
-    void getMiiHistory(s8 *miiIdxOut, u8 *radioSensOut, u32 numPlayers, u32 playerIdx) const; // 8018abe0
-    void setMiiHistory(const s8 *miiIdx, const u8 *radioSens, u32 numPlayers, u32 playerIdx); // 8018ab60
+    /**
+     * @brief Get Mii history fields
+     * @address 8018abe0
+     * @param[out] miiIdxOut For storing mii index 
+     * @param[out] radioSensOut For storing radio sensitivity
+     * @param[in] numPlayers Player count
+     * @param[in] playerIdx Index into history
+     */
+    void getMiiHistory(s8 *miiIdxOut, u8 *radioSensOut, u32 numPlayers, u32 playerIdx) const;
+    /**
+     * @brief Set Mii history fields
+     * @address 8018ab60
+     * @param[in] miiIdx Mii index 
+     * @param[in] radioSens Radio sensitivity
+     * @param[in] numPlayers Player count
+     * @param[in] playerIdx Index into history
+     */
+    void setMiiHistory(const s8 *miiIdx, const u8 *radioSens, u32 numPlayers, u32 playerIdx);
 
-    //! Access training bits in bitfield
-    bool getTrainingUnlock(u8 i) const; // 8018aa90
-    bool setTrainingUnlock(u8 i, bool val); // 8018aab8
+    /**
+     * @brief Check training unlock
+     * @note Training game bits start at 15, so this is equivalent to getBitFlag(15 + i)
+     * @see ETrainingGame
+     * @address 8018aa90
+     * @param i Training game
+     */
+    bool getTrainingUnlock(u8 i) const;
+    /**
+     * @brief Check training unlock
+     * @note Training game bits start at 15, so this is equivalent to setBitFlag(15 + i, val)
+     * @see ETrainingGame
+     * @address 8018aab8
+     * @param i Training game
+     * @param val New value
+     */
+    bool setTrainingUnlock(u8 i, bool val);
 
-    //! Access any bit in bitfield
-    bool getBitFlag(u8 i) const; // 8018aaf0
-    bool setBitFlag(u8 i, bool val); // 8018ab10
+    //! @address 8018aaf0
+    bool getBitFlag(u8 i) const;
+    //! @address 8018ab10
+    bool setBitFlag(u8 i, bool val);
 
-    //! Access last fitness test time (RPCalendarTime16)
-    u16 getLastFitnessTestTime() const; // 8018ab40
-    void setLastFitnessTestTime(u16 time); // 8018ab40
+    //! @address 8018ab40
+    u16 getLastFitnessTestTime() const;
+    //! @address 8018ab40
+    void setLastFitnessTestTime(u16 time);
 
-    //! Access number of total fitness tests
-    u8 getTotalFitnessTests() const; // 8018ab50
-    void setTotalFitnessTests(u8 num); // 8018ab58
+    //! @address 8018ab50
+    u8 getTotalFitnessTests() const;
+    //! @address 8018ab58c
+    void setTotalFitnessTests(u8 num);
 
-    //! Reset all data (From WFU)
-    void reset(); // 8018ac64
+    /**
+     * @brief Reset all data
+     * @address 8018ac64
+     */
+    void reset();
 
 private:
-    //! Mii History for every possible amount of players
+    //! @brief Mii History for every possible amount of players
     MiiHistory mHistory[1 + 2 + 3 + 4]; // at 0x0
 
-    //! Used for the Wii Message Board
+    //! @brief Total fitness tests across all Miis
     u8 mTotalFitnessTests; // at 0x46
+    //! @brief Last fitness test by any Mii
     u16 mLastFitnessTestTime; // at 0x48
 
     u16 SHORT_0x4A;
     u8 BYTE_0x4C;
 
-    //! Common save data bitfield, used primarily for training games
+    //! @brief Common save data bitfield, used primarily for training games
     u32 mBitfield;
 
     u32 WORD_0x54;
