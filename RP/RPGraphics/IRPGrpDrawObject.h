eeb41a14357eb86cca618de330d093dd23d2c985
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-02-23 14:26:07 -0500

    Document IRPGrpDrawObject

--- RP/RPGraphics/IRPGrpDrawObject.h
+++ RP/RPGraphics/IRPGrpDrawObject.h
@@ -4,19 +4,22 @@
 
 /**
  * @brief Linked-list interface for renderable objects.
- * The engine's renderer has a linked list of DrawObjects that it traverses through when rendering.
- * 
- * (Name from Wii Fit U)
+ * @details The engine's renderer has a linked list of draw objects that it iterates through when rendering.
+ * @wfuname
  */
 class IRPGrpDrawObject
 {
 private:
+    //! @brief Next draw object
     IRPGrpDrawObject *mNext; // at 0x0
 
 public:
     IRPGrpDrawObject() : mNext(NULL) {}
     virtual ~IRPGrpDrawObject() {} // at 0x4
+
+    //! @brief Any drawing the object needs to do
     virtual void UserDraw() {} // at 0x8
+    //! @brief Debug draw pass, nearly all objects stub this out
     virtual void DebugDraw() {} // at 0xC
 };
 
