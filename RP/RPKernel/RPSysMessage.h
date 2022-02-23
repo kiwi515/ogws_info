d586d5ffc1a55753ffcf49a971cc60c92950cb1d
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-02-23 11:20:41 -0500

    Document RPSysMessage

--- RP/RPKernel/RPSysMessage.h
+++ RP/RPKernel/RPSysMessage.h
@@ -5,17 +5,17 @@
 #include <egg/util/eggMsgRes.h>
 
 /**
- * @brief Wrapper class for EGG's MsgRes, but with added support
- * for the extra text features seen in RP BMG files.
- * 
- * (Name from Wii Fit U)
+ * @brief MsgRes wrapper with extra MsgInfo support
+ * @details Has added support for the extra text features
+ * seen in RP BMG files (scale, char space, etc.)
+ * @wfuname
  */
 class RPSysMessage : EGG::MsgRes
 {
 public:
     /**
-     * @brief Custom RP BMG message info block entry.
-     * MsgInfoBlock denotes the size of the block entry,
+     * @brief RP extension for MsgInfo
+     * @details MsgInfoBlock stores the sizes of its entries,
      * allowing games to add extra features to their text.
      */
     struct RPMsgInfoBlockEntry : EGG::MsgRes::MsgInfoBlockEntry
@@ -23,21 +23,42 @@ public:
         //! Text scale
         u16 mScale; // at 0x8
         //! Text kerning
-        u8 mCharSpace; // at 0xA
+        s8 mCharSpace; // at 0xA
     };
 
 public:
-    RPSysMessage(const void *bin, EGG::Heap *heap); // 80190dec
-    virtual ~RPSysMessage(); // 80190d94
+    /**
+     * @address 80190dec
+     * @param bin Binary message file (BMG)
+     */
+    RPSysMessage(const void *bin, EGG::Heap *heap);
+    //! @address 80190d94
+    virtual ~RPSysMessage();
 
-    //! Retrieve message info by searching the message ID data block
-    RPMsgInfoBlockEntry * GetMessage(u32 id1, u32 id2); // 80190d90
-    //! Retrieve attribute from message info block
-    u32 GetAttribute(u32 id1, u32 id2); // 80190d6c
-    //! Retrieve scale from message info block
-    u16 GetScale(u32 id1, u32 id2); // 80190d48
-    //! Retrieve kerning from message info block
-    u8 GetCharSpace(u32 id1, u32 id2); // 80190d24
+    /**
+     * @brief Get message info by ID
+     * @address 80190d90
+     */
+    RPMsgInfoBlockEntry * GetMessage(u32 id1, u32 id2);
+
+    /**
+     * @brief Get message attribute by ID
+     * @note Attribute is part of base MsgInfo
+     * @address 80190d6c
+     */
+    u32 GetAttribute(u32 id1, u32 id2);
+
+    /**
+     * @brief Get message scale by ID
+     * @address 80190d48
+     */
+    u16 GetScale(u32 id1, u32 id2);
+
+    /**
+     * @brief Get message char space (kerning) by ID
+     * @address 80190d24
+     */
+    s8 GetCharSpace(u32 id1, u32 id2);
 };
 
 #endif
